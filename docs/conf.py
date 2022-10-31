bc14834fde7033564008eb415b6e111c9e59dee1
sachin-suresh-rapyuta <sachin.suresh@rapyuta-robotics.com>
2022-10-31 14:36:50 +0900

    modified conf for simplepdf

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
+import os
+import sphinx
 
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2022, Rapyuta Robotics'
+author = 'Rapyuta Robotics'
 
 
 # -- General configuration
@@ -15,7 +17,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.redoc'
+    'sphinxcontrib.redoc',
+    'sphinx_simplepdf'
 ]
 
 intersphinx_mapping = {
@@ -34,29 +37,47 @@ html_theme_options = {
     'display_version': True
 }
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+simplepdf_vars = {
+    'primary-opaque': '#E11A27',
+    'cover-bg': 'url(ug_front_cover.png) no-repeat center',
+    'primary': '#E11A27',
+    'secondary': '#E11A27',
+    'cover': '#ffffff',
+    'white': '#ffffff',
+    'links': '#2980B9',
+    'top-left-content': 'counter(page)',
+    'top-center-content': '',
+    'top-right-content': '"PA-AMR Documentation"',
+    'bottom-left-content': 'counter(page)',
+    'bottom-center-content': '"Rapyuta Robotics"',
+    'bottom-right-content': 'string(heading)'
+}
 
-#Open API Spec to render APIs
-#redoc = [
-#    {
-#        'name': 'Batcomputer API',
-#        'page': 'index',
-#        'spec': 'specs/openapi.yaml',
-#        'embed': True,
-#    }
-#]
+plantuml_output_format = "svg_img"
+local_plantuml_path = os.path.join(os.path.dirname(__file__), "../", "docs", "utils", "plantuml.jar")
+plantuml = f"java -Djava.awt.headless=true -jar {local_plantuml_path}"
 
-# -- Extension configuration -------------------------------------------------
 
-# add sourcecode to path
-import sys, os
-sys.path.insert(0, os.path.abspath('../src'))
- 
- 
-############################
-# SETUP THE RTD LOWER-LEFT #
-############################
+def setup_jquery(app, exception):
+    """
+    Inject jQuery if Sphinx>=6.x
+
+    Staring on Sphinx 6.0, jQuery is not included with it anymore.
+    As this extension depends on jQuery, we are including it when Sphinx>=6.x
+    """
+
+    if sphinx.version_info >= (5, 0, 0):
+        # https://jquery.com/download/#using-jquery-with-a-cdn
+        jquery_cdn_url = "https://code.jquery.com/jquery-3.6.0.min.js"
+        html_js_files = getattr(app.config, "html_js_files", [])
+        html_js_files.append((
+            jquery_cdn_url,
+            {
+                'integrity': 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=',
+                'crossorigin': 'anonymous'
+            }
+        ))
+        app.config.html_js_files = html_js_files
 
 # The short X.Y version.
 version = '6.16.1-dev'
